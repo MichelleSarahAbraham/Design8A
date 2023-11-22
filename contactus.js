a81eafa4feed548caacc2885c505ea1f0ed90d51
MichelleSarahAbraham <151709917+MichelleSarahAbraham@users.noreply.github.com>
2023-11-22 11:01:15 +0530

    Add files via upload

---
+++ contactus.js
@@ -0,0 +1,23 @@
+﻿function validateForm(){
+	var name = document.contactus.Name;
+	var email = document.contactus.Email;
+	var grade = document.contactus.Grade;
+	var count = 0;
+	if (name.value === "") {
+	name.placeholder="Please fill this field";
+	name.style.border = "2px solid red";
+	count++;
+	}
+   if (email.value === "") {
+	email.placeholder="Please fill this field";
+	email.style.border = "2px solid red";
+	count++;
+	}
+	if (grade.value === "") {
+	grade.placeholder="Please fill this field";
+	grade.style.border = "2px solid red";
+	count++;
+	}
+	if(count>0)
+	  return false;
+}
